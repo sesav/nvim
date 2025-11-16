82c74e51d40175a581118d8773272d43f44f4614
Sergey Silaev <hello@8am.dev>
2025-11-17 00:56:09 +0100

    Update after/ftplugin/rust.lua

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,9 +1,14 @@
--- Set the maximum text width for wrapping
 vim.o.textwidth = 99
 
 -- Keybindings for Rust development
-vim.keymap.set("n", "<C-b>", "<cmd>Cargo build<CR>") -- Run Cargo build
-vim.keymap.set("n", "<Leader>x", "<cmd>RustRun<CR>") -- Run Rust code
+vim.keymap.set("n", "<C-b>", "<cmd>Cargo build<CR>")
+vim.keymap.set("n", "<Leader>x", "<cmd>Cargo run<CR>")
+vim.keymap.set("n", "<Leader>f", "<cmd>RustFmt<CR>")
 
--- Abbreviation for Rust: "dd" becomes "#[derive(Debug)]"
+-- Tests
+vim.keymap.set("n", "<Leader>rt", "<cmd>RustTest<CR>")
+vim.keymap.set("n", "<Leader>tt", "<cmd>DebugTest<CR>")
+
+-- ia
 vim.cmd("iabbrev dd #[derive(Debug)]")
+vim.cmd("iabbrev cmt cmt<Leader>t<Left>")
