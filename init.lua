f3ea0c637a77b194fda04c71730e4074000103a8
Sergey Silaev <hello@8am.dev>
2026-01-02 15:23:20 +0100

    Update nvim-treesitter opts, add .editorconfig

--- init.lua
+++ init.lua
@@ -970,7 +970,7 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+    main = "nvim-treesitter", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
