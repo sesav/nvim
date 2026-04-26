d7b46aa99971e849e6a0629fcea3134db95d2345
Sergey Silaev <hello@8am.dev>
2026-04-26 14:25:02 +0200

    Override the gd keymap to pass empty file_ignore_patterns for LSP nav

--- init.lua
+++ init.lua
@@ -419,7 +419,9 @@ require("lazy").setup({
           -- Jump to the definition of the word under your cursor.
           -- This is where a variable was first declared, or where a function is defined, etc.
           -- To jump back, press <C-t>.
-          vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = buf, desc = "[G]oto [D]efinition" })
+          vim.keymap.set("n", "gd", function()
+            builtin.lsp_definitions({ file_ignore_patterns = {} })
+          end, { buffer = buf, desc = "[G]oto [D]efinition" })
 
           -- Fuzzy find all the symbols in your current document.
           -- Symbols are things like variables, functions, types, etc.
@@ -1017,7 +1019,6 @@ require("lazy").setup({
     end,
   },
 
-  
   -- aerial
   -- https://github.com/stevearc/aerial.nvim
   {
